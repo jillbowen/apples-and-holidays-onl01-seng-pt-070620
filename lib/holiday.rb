1776bddbe61bdf2956d7f10c8ddbfb64d90fdd5e
Jill Short <jillian.b.short@gmail.com>
2020-07-19 05:57:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
- 
+ holiday_hash.each do |season, holidays|
+   puts "#{season.to_s.capitalize}"
+   holidays.each do |holiday, supplies|
+     puts "  #{holiday.to_s.split(" ").map {|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
