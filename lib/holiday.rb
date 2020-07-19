77d2d81437fbb4e75108fdc85d21000050db2c65
Jill Short <jillian.b.short@gmail.com>
2020-07-19 05:57:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,6 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
    puts "#{season.to_s.capitalize}"
    holidays.each do |holiday, supplies|
      puts "  #{holiday.to_s.split(" ").map {|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    end
    end
  end
 end
